6c82eb8d2871e9efc6c8da70c01ae103f46ce5ef
Murat <muratesen2345@gmail.com>
2024-08-26 16:07:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Currency from "./components/Currency";
+
+function App() {
+  return (
+    <div style={{display:'flex' , flexDirection: 'column', justifContent: 'center' , alignItems: 'center'}} >
+      <Currency />
+    </div>
+  );
+}
+
+export default App;
